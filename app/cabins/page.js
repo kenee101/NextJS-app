c64149ededc50071f4683412d4c1035e6d5f09e9
Usih Elijah <princeusih@gmail.com>
2024-05-23 00:03:46 +0100

    FirstCommit

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,11 @@
+export const metadata = {
+  title: "Cabins",
+};
+
 export default function Page() {
-  return <h1>Cabins page</h1>;
+  return (
+    <div>
+      <h1>Cabins page</h1>
+    </div>
+  );
 }
