2cd79f466d756e4759c451d25fcf43f29d330bc0
Usih Elijah <princeusih@gmail.com>
2024-05-23 22:58:45 +0100

    SecondCommit

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,12 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
+import Header from "./_components/Header";
 
+import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
   display: "swap",
 });
-
-import "@/app/_styles/globals.css";
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -24,15 +22,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+        <Header />
 
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
