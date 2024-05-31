f33eac73b62a7a3d82489746171250d653d01498
kenee101 <elijah.usih@stu.cu.edu.ng>
2024-05-31 14:51:09 +0100

    SixthCommit

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { updateGuest } from "../_lib/actions";
 import Image from "next/image";
-import { useFormStatus } from "react-dom";
+import Button from "@/app/_components/SubmitButton";
 
 function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState();
@@ -59,22 +59,9 @@ function UpdateProfileForm({ children, guest }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <Button pendingLabel="Updating...">Update Profile</Button>
       </div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-
-  return (
-    <button
-      disabled={pending}
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
   );
 }
 
